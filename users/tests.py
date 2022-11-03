beca90632befd9444d845adbc3e067ab0ffb3922
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-11-03 17:46:56 +0900

    Create TestUserModel

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from typing import Final
+from django.db import IntegrityError
+from rest_framework.test import APITestCase
+from .models import User
 
-# Create your tests here.
+
+class TestUserModel(APITestCase):
+    def test_create_user(self):
+        try:
+            self.unvailded_email_user = User.objects.create(
+                username="unvailded_username",
+                email="unvailded_email",
+                password="password",
+            )
+        except IntegrityError:
+            pass
+        except Exception as e:
+            raise e
+        else:
+            AssertionError("unvailded_email_user is created.")
+        VALIDED_EMAIL: Final = "valided_email@valided_email.com"
+        user = User.objects.create(
+            username="valided_username",
+            email=VALIDED_EMAIL,
+            password="password",
+        )
+        print("test_create_user", user.username)
+        self.assertEqual(user.email, VALIDED_EMAIL)
