41e140b476f74cec6c8294520416349396c94f35
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-11-03 15:58:59 +0900

    Fix config
    환경 변수 및 현재 사용 불가능한 url 주석 처리

--- config/settings.py
+++ config/settings.py
@@ -9,26 +9,31 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 import environ
 from pathlib import Path
 
 env = environ.Env(
     # set casting, default value
-    DEBUG=(bool, False)
+    DEBUG=(bool, True)
 )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Read .env file and set environment variables
+environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -53,7 +58,7 @@ THIRD_PARTY_APPS = [
 CUSTOM_APPS = [
     # 커스템 앱
     "users.apps.UsersConfig",
-    "ledger.apps.LedgerConfig",
+    "ledgers.apps.LedgersConfig",
 ]
 
 INSTALLED_APPS = SYSTEM_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
