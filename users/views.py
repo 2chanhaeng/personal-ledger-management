b8deb5467107e9a218b404334bd6f65857ade3b2
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-11-04 14:33:20 +0900

    Fix views
    SignUpView 주석 수정 및 성공시 201 응답
    LoginView 이메일 조회 후 사용자가 없으면 404, 비밀번호가 틀리면 400 응답
    LogoutView post에서 get으로 변경

--- users/views.py
+++ users/views.py
@@ -1,11 +1,11 @@
 from typing import Final
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login, logout
 from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ParseError
-from rest_framework.status import HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_201_CREATED, HTTP_404_NOT_FOUND
 from .models import User
 from .serializers import UserSerializer
 
@@ -14,7 +14,7 @@ class SignUpView(APIView):
     def post(self, request: Request) -> Response:
         """
         회원가입
-        POST /api/v1/users
+        POST /api/v1/users/signup/
         """
 
         password: Final = request.data.get("password")
@@ -27,7 +27,7 @@ class SignUpView(APIView):
         user.set_password(password)
         user.save()
         serializer = UserSerializer(user)
-        return Response(serializer.data)
+        return Response(serializer.data, status=HTTP_201_CREATED)
 
 
 class LoginView(APIView):
@@ -38,13 +38,22 @@ class LoginView(APIView):
         """
 
         email: Final = request.data.get("email")
+        if not email:
+            raise ParseError("Email is required.")
         password: Final = request.data.get("password")
-        if not email or not password:
-            raise ParseError("Username or password is required.")
-        user: Final = authenticate(request, email=email, password=password)
-        if not user:
+        if not password:
+            raise ParseError("Password is required.")
+        # request.data["username"] = email
+        filtered_user: Final = User.objects.filter(email=email)
+        if not filtered_user.exists():
             return Response(
-                {"detail": "Invalid user data."},
+                {"detail": f"User with email {email} does not exist."},
+                status=HTTP_404_NOT_FOUND,
+            )
+        user: Final = filtered_user.first()
+        if not user.check_password(password):
+            return Response(
+                {"detail": "Password is not correct."},
                 status=HTTP_400_BAD_REQUEST,
             )
         login(request, user)
@@ -56,10 +65,10 @@ class LogoutView(APIView):
 
     permission_classes = [IsAuthenticated]
 
-    def post(self, request: Request) -> Response:
+    def get(self, request: Request) -> Response:
         """
         로그아웃
-        POST /api/v1/users/logout
+        GET /api/v1/users/logout
         """
         logout(request)
         return Response({"status": True})
