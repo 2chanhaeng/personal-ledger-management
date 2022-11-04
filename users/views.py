ee88a3215c98465dc619a3674c219088cf2ea74b
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-11-04 16:18:52 +0900

    Refactor views applying jwt
    로그인 시 쿠키에 refresh, access 토큰을 저장
    로그아웃 시 refresh 토큰을 받아 토큰 만료

--- users/views.py
+++ users/views.py
@@ -1,11 +1,16 @@
 from typing import Final
-from django.contrib.auth import login, logout
 from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ParseError
-from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_201_CREATED, HTTP_404_NOT_FOUND
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_201_CREATED,
+    HTTP_404_NOT_FOUND,
+)
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import User
 from .serializers import UserSerializer
 
@@ -51,24 +56,42 @@ class LoginView(APIView):
                 status=HTTP_404_NOT_FOUND,
             )
         user: Final = filtered_user.first()
-        if not user.check_password(password):
+        if user.password != password:
             return Response(
-                {"detail": "Password is not correct."},
+                {"detail": f"Password is not correct. {password}, {user.password}"},
                 status=HTTP_400_BAD_REQUEST,
             )
-        login(request, user)
-        # TODO: create JWT token and response
-        return Response({"status": True})
+        token: Final[RefreshToken] = RefreshToken.for_user(user)
+        refresh: Final[RefreshToken] = str(token)
+        access: Final = str(token.access_token)
+        response = Response({"user": user.id})
+        response.set_cookie(key="refresh", value=refresh, httponly=True)
+        response.set_cookie(key="access", value=access, httponly=True)
+        return response
 
 
 class LogoutView(APIView):
-
+    authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
-    def get(self, request: Request) -> Response:
+    def post(self, request: Request) -> Response:
         """
         로그아웃
-        GET /api/v1/users/logout
+        POST /api/v1/users/logout
         """
-        logout(request)
-        return Response({"status": True})
+
+        # get token from request cookie
+        refresh: Final = request.data.get("refresh")
+        if not refresh:
+            raise ParseError("Refresh token is required.")
+        # delete token from database
+        try:
+            token: Final = RefreshToken(refresh)
+            token.blacklist()
+        except Exception:
+            raise ParseError("Refresh token is invalid.")
+        # delete token from cookie
+        response = Response({"status": True})
+        response.delete_cookie("refresh")
+        response.delete_cookie("access")
+        return response
