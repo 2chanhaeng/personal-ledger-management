beeb41c11c6bc7e68fa19cafa9854ae551b2a4a8
ChanHaeng Lee <2chanhaeng@gmail.com>
2022-11-06 22:06:34 +0900

    Create LedgerDetailSerializer
    가계부 개별 조회 Serializer 생성

--- ledgers/serializers.py
+++ ledgers/serializers.py
@@ -7,3 +7,10 @@ class LedgersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ledger
         fields = ("amount", "memo", "created_at")
+
+
+class LedgerDetailSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Ledger
+        fields = ("amount", "memo", "info", "created_at", "deleted_at")
